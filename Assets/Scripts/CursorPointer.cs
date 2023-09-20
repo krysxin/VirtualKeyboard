412d32947755166cad532602d30bf171471919b5
filvan1 <filip.vannfalt@gmail.com>
2023-09-20 17:09:52 +0200

    Cursor now changes intensity based on distance

--- Assets/Scripts/CursorPointer.cs
+++ Assets/Scripts/CursorPointer.cs
@@ -9,36 +9,47 @@ namespace HandTracking
   {
     public int NumFingers = 1;
     public GameObject CursorPrefab;
+    public Material CursorMaterial;
     private GameObject[] _cursor;
 
     override protected void Start()
     {
       _cursor = new GameObject[NumFingers];
+      _cursor[0] = GameObject.Instantiate(CursorPrefab);
+      _cursor[0].SetActive(false);
       base.Start();
     }
 
     override protected void Update()
     {
+      _cursor[0].SetActive(base.pointerActive);
       base.Update();
     }
 
     Transform getCanvas(Transform element)
     {
-      Canvas[] c = element.GetComponentsInChildren<Canvas>();
-      if (c.Length == 0) return null;
+      Transform result = null;
+      Canvas c = element.GetComponentInChildren<Canvas>();
+      if (c != null)
+      {
+        result = c.gameObject.transform;
+      }
+      c = element.GetComponentInParent<Canvas>();
+      if (c != null)
+      {
+        Debug.Log(c.gameObject);
+        result = c.gameObject.transform;
+      }
 
-      return c[0].gameObject.transform;
+      return result;
     }
 
     override protected void Interact(PointerHitInfo hitInfo)
     {
+
       if (hitInfo.hitTransform != null)
       {
         var canvas = getCanvas(hitInfo.hitTransform);
-        if (_cursor[0] == null)
-        {
-          _cursor[0] = GameObject.Instantiate(CursorPrefab);
-        }
         if (canvas != null)
         {
           _cursor[0].transform.SetParent(canvas);
@@ -46,11 +57,15 @@ namespace HandTracking
         }
 
 
-        _cursor[0].transform.position = hitInfo.position + new Vector3(0, 0, -0.01f);
-
       }
 
-      // Move cursor slightly in front of hit object
+      if (hitInfo.validHit)
+      {
+        _cursor[0].transform.position = hitInfo.position + new Vector3(0, 0, -0.01f);
+        CursorMaterial.SetFloat("_Distance", hitInfo.rayDistance);
+      }
+
+
 
       base.Interact(hitInfo);
     }
