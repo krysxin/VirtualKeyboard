187c376b6a119c55a7e65343febf429bda642b0f
GoBigxs <849782050@qq.com>
2023-10-02 16:12:41 +0200

    add canvas keyboard

---
+++ Assets/Keypad/Script/Passcode.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+public class Passcode : MonoBehaviour
+{
+    string Code = "456";
+    string Nr = null;
+    int NrIndex = 0;
+    string alpha;
+    public Text UiText = null;
+
+    public void CodeFunction(string Numbers)
+    {
+        NrIndex++;
+        Nr = Nr + Numbers;
+        UiText.text = Nr;
+
+    }
+    public void Enter()
+    {
+        if (Nr == Code)
+        {
+            SceneManager.LoadScene(1);
+            Debug.Log("Matched");
+        }
+    }
+    public void Delete()
+    {
+        NrIndex++;
+        Nr = null;
+        UiText.text = Nr;
+    }
+}
