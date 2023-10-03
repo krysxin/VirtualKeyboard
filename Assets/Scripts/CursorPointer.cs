8ca8a6c47fd2d11dd3780f73424cc7b316f6d7db
filvan1 <filip.vannfalt@gmail.com>
2023-10-03 11:15:58 +0200

    multiple finger tracking

--- Assets/Scripts/CursorPointer.cs
+++ Assets/Scripts/CursorPointer.cs
@@ -7,22 +7,29 @@ namespace HandTracking
 
   public class CursorPointer : Pointer
   {
-    public int NumFingers = 1;
     public GameObject CursorPrefab;
     public Material CursorMaterial;
-    private GameObject[] _cursor;
+    private GameObject[] _cursors;
 
     override protected void Start()
     {
-      _cursor = new GameObject[NumFingers];
-      _cursor[0] = GameObject.Instantiate(CursorPrefab);
-      _cursor[0].SetActive(false);
+      _cursors = new GameObject[5];
+      for (int i = 0; i < 5; i++)
+      {
+        _cursors[i] = GameObject.Instantiate(CursorPrefab);
+        _cursors[i].SetActive(false);
+      }
+
       base.Start();
     }
 
     override protected void Update()
     {
-      _cursor[0].SetActive(base.pointerActive);
+      for (int i = 0; i < 5; i++)
+      {
+        _cursors[i].SetActive(base.pointerActive);
+      }
+
       base.Update();
     }
 
@@ -46,14 +53,14 @@ namespace HandTracking
 
     override protected void Interact(PointerHitInfo hitInfo)
     {
-
+      int fingerIndex = hitInfo.fingerIndex;
       if (hitInfo.hitTransform != null)
       {
         var canvas = getCanvas(hitInfo.hitTransform);
         if (canvas != null)
         {
-          _cursor[0].transform.SetParent(canvas);
-          _cursor[0].transform.localScale = Vector3.one;
+          _cursors[fingerIndex].transform.SetParent(canvas);
+          _cursors[fingerIndex].transform.localScale = Vector3.one;
         }
 
 
@@ -61,7 +68,7 @@ namespace HandTracking
 
       if (hitInfo.validHit)
       {
-        _cursor[0].transform.position = hitInfo.position + new Vector3(0, 0, -0.01f);
+        _cursors[fingerIndex].transform.position = hitInfo.position + new Vector3(0, 0, -0.01f);
         CursorMaterial.SetFloat("_Distance", hitInfo.rayDistance);
       }
 
